de84cbcf4fd903a8f4e9e85a220739f0e0741124
Jaime Martinez-Diaz <jm21558@gmail.com>
2021-04-07 09:24:32 -0400

    Fixed the pinConfig header

--- main.c
+++ main.c
@@ -4,30 +4,7 @@
  *
  * Created on July 23, 2020, 12:34 PM
  */
-// CONFIG1
-#pragma config FOSC = INTOSC    //  (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
-#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-
-// CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config PPS1WAY = OFF     // Peripheral Pin Select one-way control (The PPSLOCK bit cannot be cleared once it is set by software)
-#pragma config ZCD = OFF        // Zero Cross Detect Disable Bit (ZCD disable.  ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
-#pragma config PLLEN = ON       // PLL Enable Bit (4x PLL is always enabled)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
-#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
-
-// CONFIG3
-#pragma config WDTCPS = WDTCPS1F// WDT Period Select (Software Control (WDTPS))
-#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-#pragma config WDTCWS = WDTCWSSW// WDT Window Select (Software WDT window size control (WDTWS bits))
-#pragma config WDTCCS = SWC     // WDT Input Clock Selector (Software control, controlled by WDTCS bits)
-#include <xc.h>
+#include "pinConfig.h"
 
 #define RED_LED_TRIS    TRISCbits.TRISC0
 #define BLUE_LED_TRIS   TRISCbits.TRISC1
@@ -67,7 +44,6 @@
 
 #include <stdint.h>
 #include <math.h>
-#include <Setup.h>
 
 const uint8_t red_tone = 38;        //200Hz
 const uint8_t blue_tone = 25;       //300Hz
---
+++ pinConfig.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   pinConfig.h
+ * Author: Jaime
+ *
+ * Created on April 7, 2021, 9:22 AM
+ */
+
+#ifndef PINCONFIG_H
+#define	PINCONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+// CONFIG1
+#pragma config FOSC = INTOSC    //  (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PPS1WAY = OFF     // Peripheral Pin Select one-way control (The PPSLOCK bit cannot be cleared once it is set by software)
+#pragma config ZCD = OFF        // Zero Cross Detect Disable Bit (ZCD disable.  ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PLLEN = ON       // PLL Enable Bit (4x PLL is always enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS1F// WDT Period Select (Software Control (WDTPS))
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config WDTCWS = WDTCWSSW// WDT Window Select (Software WDT window size control (WDTWS bits))
+#pragma config WDTCCS = SWC     // WDT Input Clock Selector (Software control, controlled by WDTCS bits)
+#include <xc.h>
+
+#endif	/* PINCONFIG_H */
+
