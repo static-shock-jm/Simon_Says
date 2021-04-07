4e8a06b51585637dbfdcaf72f52f0814be7acbe2
Jaime Martinez-Diaz <jm21558@gmail.com>
2021-04-07 09:52:17 -0400

    Moved PWM to new header, attempt 2

--- main.c
+++ main.c
@@ -5,12 +5,13 @@
  * Created on July 23, 2020, 12:34 PM
  */
 #include "pinConfig.h"
+#include "PWM.h"
 
 #define RED_LED_TRIS    TRISCbits.TRISC0
 #define BLUE_LED_TRIS   TRISCbits.TRISC1
 #define YELLOW_LED_TRIS TRISCbits.TRISC2
 #define GREEN_LED_TRIS  TRISAbits.TRISA4
-#define BUZZER_TRIS     TRISCbits.TRISC5             
+             
 
 #define RED_LED     PORTCbits.RC0
 #define BLUE_LED    PORTCbits.RC1
@@ -224,36 +225,7 @@ void init_debounce_TMR4(void){
     IOCCP4 = 1;
 }
 
-void init_PWM_TMR2(void){            //USIING TIMER2
-    //------------------------RC5 PWM out to BUZZER----------------------------
-         
-    BUZZER_TRIS = 1;       //SET PORT C5 TO INPUT
-    RC5PPS = 0b01100;           //PPS FOR CCP1 TO RC5
-    CCPTMRSbits.CCP1TSEL = 0b00; // CCP1 is based off of TIMER2 in PWM mode
-    
-    PR2 = red_tone;                   ////sets pwm period TO GET 25mS
-    
-    CCP1CONbits.MODE = 0b1100;  //SET CCP TO PWM MODE
-    CCP1CONbits.FMT = 0;        //DUTY CYCLE ALIGNMENT TO THE RIGHT
-    CCP1CONbits.EN =1;          //TURN ON
-    
-    CCPR1H = 0b00;              //MSB
-    CCPR1L = tone_off;        //LSB
-    
-    PIR1bits.TMR2IF = 0;        //Disabling the interrupt flag of the timer 
-    T2CONbits.CKPS = 0b111;     //pre scalar: 128
-    T2CONbits.OUTPS = 0x0;      //post scalar
-    T2CONbits.ON =1;            //SET TIMER2 ON BIT 
-    T2CLKCONbits.CS = 0x0;      //select FOSC/4
-    T2HLTbits.PSYNC = 1;
-    T2HLTbits.T2CKSYNC =1;   
-    T2HLTbits.MODE = 0b0000;
-    
-    while(PIR1bits.TMR2IF == 1 & TMR2 ==0){
-        PIR1bits.TMR2IF = 0;
-    }
-    BUZZER_TRIS = 0;
-}
+
 
 void init_delay_TMR1(void){
     //TIMER1 FOR DEBOUNCING*********************LATER ON, PERFECT
--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c PWM.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/PWM.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/PWM.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/PWM.p1
 
 # Source Files
-SOURCEFILES=main.c
+SOURCEFILES=main.c PWM.c
 
 
 
@@ -102,6 +102,14 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/PWM.p1: PWM.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PWM.p1.d 
+	@${RM} ${OBJECTDIR}/PWM.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/PWM.p1 PWM.c 
+	@-${MV} ${OBJECTDIR}/PWM.d ${OBJECTDIR}/PWM.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/PWM.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -110,6 +118,14 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/PWM.p1: PWM.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PWM.p1.d 
+	@${RM} ${OBJECTDIR}/PWM.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c    -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/PWM.p1 PWM.c 
+	@-${MV} ${OBJECTDIR}/PWM.d ${OBJECTDIR}/PWM.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/PWM.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
